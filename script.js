1c1ae1146efffa61504dbef5bc0d9bae370ce63f
Mazid <mazidyerima2@gmail.com>
2026-06-25 19:45:35 +0000

    I was able to recreate the JS today. Things I learnt: createElement, setAttribute, appendChild

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 const myInput = document.getElementById("input");
 const myButton = document.getElementById("button");
-const taskLabel = document.getElementById("task-label");
 const myCard = document.getElementById("todo-card");
+
+let newDiv = document.createElement("div");
+let newCheckbox = document.createElement("input");
+let newLabel = document.createElement("label");
+
 
 
 myButton.onclick = function(){
-    newTask = myInput.value;
+    task = myInput.value;
 
-    const newDiv = document.createElement("div");
-    const newCheckbox = document.createElement("input");
-    const newLabel = document.createElement("label");
+    if(task != ""){
+        let newDiv = document.createElement("div");
+        let newCheckbox = document.createElement("input");
+        let newLabel = document.createElement("label");
 
-    newLabel.textContent = newTask;
-    newCheckbox.setAttribute('type', 'checkbox');
+        
+        newLabel.textContent = task;
 
-    newLabel.setAttribute('id', 'task-label');
-    newCheckbox.setAttribute('id', 'checkbox')
 
-    newDiv.appendChild(newCheckbox);
-    newDiv.appendChild(newLabel);
+        newCheckbox.setAttribute('type', 'checkbox');
+        newCheckbox.setAttribute('id', 'checkbox');
+        newLabel.setAttribute('id', 'task-label');
+
+        
+        newDiv.appendChild(newCheckbox);
+        newDiv.appendChild(newLabel);
+        myCard.appendChild(newDiv);
+
+        myInput.value = "";
+    }
+
+    
+    
     
 
-    myCard.appendChild(newDiv);
     
-    myInput.value = '';
-    
+
+
 
 }
