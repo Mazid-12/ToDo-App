14e94b2ecb2f6fafc9df7dce3be211b0b5294e0a
Mazid <mazidyerima2@gmail.com>
2026-06-27 19:31:11 +0000

    I just added delete buttons to each new taks

--- script.js
+++ script.js
@@ -12,32 +12,37 @@ myButton.onclick = function(){
     task = myInput.value;
 
     if(task != ""){
-        let newDiv = document.createElement("div");
-        let newCheckbox = document.createElement("input");
-        let newLabel = document.createElement("label");
+        const newDiv = document.createElement("div");
+        const newCheckbox = document.createElement("input");
+        const newLabel = document.createElement("label");
+        const deleteBtn = document.createElement("button");
 
         
         newLabel.textContent = task;
+
+        deleteBtn.textContent = 'delete';
 
 
         newCheckbox.setAttribute('type', 'checkbox');
         newCheckbox.setAttribute('id', 'checkbox');
         newLabel.setAttribute('id', 'task-label');
+        newCheckbox.setAttribute('value', 'checked');
+        deleteBtn.setAttribute('id', 'deleteBtn');
+
 
         
         newDiv.appendChild(newCheckbox);
         newDiv.appendChild(newLabel);
+        newDiv.appendChild(deleteBtn);
         myCard.appendChild(newDiv);
 
         myInput.value = "";
+
+       
+
+
+
     }
 
-    
-    
-    
-
-    
-
-
-
 }
+
